fbd86796d82e069f132a7be1ee9a9baf8772241a
antonh <antonh@backbase.com>
2016-10-24 15:36:03 +0300

    Initial commit

---
+++ src/main/java/Servlet.java
@@ -0,0 +1,16 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by a.haman on 17/10/2016.
+ */
+public class Servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().print("<h1>Hello world!</h1>");
+    }
+}
